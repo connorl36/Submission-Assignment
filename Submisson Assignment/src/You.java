ebe9f48d950b4f927b19243597767461ecccd0af
connorl36 <51222904+connorl36@users.noreply.github.com>
2019-06-06 13:26:57 -0400

    Completed Submission-Assignment

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -1,6 +1,6 @@
 /**
  * 
- * @author //Put your first and last name here
+ * @author //Connor Lum
  * 
  * 
  * Assignment Instructions:
@@ -18,13 +18,37 @@
  *  - Add achiu-frhsd as a collaborator
  *
  */
-public class You{
+public class You
+{
 
 	private String firstName;
 	private String lastName;
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
+	public You()
+	{
+		firstName = "Connor";
+		lastName = "Lum";
+		favoriteAnimal = "Cat";
+		age = 17;
+	}
 	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal = animal;
+		return favoriteAnimal;
+	}
+	
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday!");
+		age++;
+		return age;
+	}
 }
